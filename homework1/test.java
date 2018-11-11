b7a7df5ecb590b51edd193471cbee109acaa6d7f
Protyush <protyushacs@gmail.com>
2018-11-11 17:07:53 +0100

    Protyush comitted second solution

---
+++ homework1/test.java
@@ -0,0 +1,11 @@
+public class test
+{
+ public static void main(String[] args)
+ {
+    System.out.println(Long.MAX_VALUE);
+    System.out.println((long)Integer.MAX_VALUE);
+    System.out.println((long)Integer.MAX_VALUE/2);
+    long[] arr=new long[(int)(Long.MAX_VALUE/2)] ;
+    System.out.println(arr.length);
+  }
+}
